a340e48d98bc0bcd5254e4c2f16639f3f7b9f442
tkadauke <tkadauke@60cb0372-8f51-4c35-923c-d3d8de73799b>
2007-09-24 14:21:50 +0000

    fuck internet explorer to death in its fucking asshole
    git-svn-id: file:///var/local/svn/emma@2342 60cb0372-8f51-4c35-923c-d3d8de73799b

--- core/widget.js
+++ core/widget.js
@@ -15,22 +15,36 @@ var Widget = Class.create({
 Object.extend(Widget, {
   ApplyBehaviours: function(parent, behaviours) {
     var objects = new Array();
-    
-    $A($(parent || document.body).getElementsByTagName("*")).each(function(element) {
-      $(element).classNames().select(function(c) { return new String(c).startsWith("thc2-"); }).each(function(className) {
-        var mapping = behaviours[className];
-        if (mapping && CurrentPage.find(element, className).length == 0) {
-          try {
-            var obj = new mapping.klass(element);
-            obj.behaviour = className;
-          } catch(e) {
-            Logger.error("Could not create class " + className + ", error: " + e.message);
-            return;
-          }
-          objects.push(obj);
+    var debugshit = "";
+    try {
+      $A($(parent || document.body).getElementsByTagName("*")).each(function(element) {
+        debugshit += "Ausseneach: "+element.className + "\n";
+        var a = $(element);
+        debugshit += 'a ' + Object.inspect(a) + "\n";
+        if (Object.inspect(a) != '[object]') {
+          var b = a.classNames();
+          debugshit += 'b ' + Object.inspect(b) + "\n";
+          var c = b.select(function(c) { return new String(c).startsWith("thc2-"); });
+          c.each(function(className) {
+            debugshit += "inneneach" + "\n";
+            var mapping = behaviours[className];
+            if (mapping && CurrentPage.find(element, className).length == 0) {
+              try {
+                var obj = new mapping.klass(element);
+                obj.behaviour = className;
+              } catch(e) {
+                Logger.error("Could not create class " + className + ", error: " + e.message);
+                return;
+              }
+              objects.push(obj);
+            }
+          });
         }
       });
-    });
-    return objects;
+      return objects;
+    } catch(e) {
+      //alert(e);
+      //alert(debugshit);
+    }
   }
 });
