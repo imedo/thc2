ce20d8df5ed62c6d4beccdf7adc69acf1a543fce
tkadauke <tkadauke@60cb0372-8f51-4c35-923c-d3d8de73799b>
2007-11-15 04:25:33 +0000

    click tracker plugin
    git-svn-id: file:///var/local/svn/emma@2771 60cb0372-8f51-4c35-923c-d3d8de73799b

---
+++ core/click_tracker.js
@@ -0,0 +1,18 @@
+var ClickTracker = Singleton.create({
+  initialize: function() {
+    this.url = '/click_tracker/track/'
+  },
+  
+  attach: function() {
+    $A(document.getElementsByTagName('a')).each(function(element) {
+      element.observe('click', function() { this.track(element) }.bind(this));
+    }.bind(this));
+  },
+  
+  track: function(element) {
+    new Ajax.Request(this.url + element.id, {
+      asynchronous:true,
+      evalScripts:false
+    });
+  }
+});
