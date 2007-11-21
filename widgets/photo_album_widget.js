e87e62fbf867c6ad873d788dbe3d055979a34e1a
tkadauke <tkadauke@60cb0372-8f51-4c35-923c-d3d8de73799b>
2007-11-21 23:34:28 +0000

    fancy photo album caroussel widget; newsletter update
    git-svn-id: file:///var/local/svn/emma@2823 60cb0372-8f51-4c35-923c-d3d8de73799b

--- widgets/photo_album_widget.js
+++ widgets/photo_album_widget.js
@@ -1,5 +1,5 @@
 /*
-  This Widget adds search dropdowns to profile links.
+  This Widget provides a fancy photo album browser.
    (c) 2007 imedo GmbH
  
   This file is freely distributable under the terms of an MIT-style license.
@@ -7,37 +7,75 @@
 */
 
 var PhotoAlbumWidget = Class.create(Widget, {
-  numPhotos: 5,
+  numPhotos: 11,
+  currentIndex: 0,
   initialize: function(element) {
     PhotoAlbumWidget.self = this;
     Widget.prototype.initialize.apply(this, arguments);
+    
+    this.measurer = new Element('div', { style: "visibility:hidden; position:absolute" });
+    document.body.insert(this.measurer);
+    
+    this.ul = $(this.element.getElementsByTagName('ul')[0]);
+    
     this.getDimensions();
-    this.photos = $A(this.element.getElementsByTagName('li'));
+
+    this.photos = $A(this.ul.getElementsByTagName('li'));
     var index = 0;
     this.photos.each(function(photo) {
       photo.index = index;
-      photo.img = $(photo).down('img');
-      photo.a = $(photo).down('a');
-      var size = photo.img.getDimensions();
-      photo.ratio = 3/4; //size.width / size.height;
-      photo.style.left = this.left + this.width + "px";
-      photo.style.top = this.top + (this.height / 2) + "px";
-      photo.style.width = "0px";
-      photo.style.height = "0px";
-      $(photo.img).addClassName('reflect');
-      
-      photo.a.observe('click', function(event) { this.photoClick(photo, event) }.bindAsEventListener(this));
+      if ($(photo).hasClassName('selected')) this.currentIndex = photo.index;
+    
+      photo.img = $(photo.getElementsByTagName('img')[0]);
+      photo.a = $(photo.getElementsByTagName('a')[0]);
+    
+      photo.style.left = this.width + "px";
+      photo.style.top = (this.height / 2) + "px";
+    
+      if (!Prototype.Browser.IE) {
+        photo.img.observe('load', function() { this.photoLoaded(photo); }.bind(this));
+      }
+      Event.observe(photo.a, 'click', function(event) { this.photoClick(photo, event) }.bindAsEventListener(this));
       index++;
     }.bind(this));
-    this.currentIndex = 0;
+    
+    // find controls
+    var controls = $A(this.element.getElementsByClassName('control'));
+    controls.each(function(control) {
+      if (control.hasClassName('prev')) {
+        Event.observe(control, 'click', (function(event) { this.moveLeft(); Event.stop(event); }).bindAsEventListener(this));
+      } else {
+        Event.observe(control, 'click', (function(event) { this.moveRight(); Event.stop(event); }).bindAsEventListener(this));
+      }
+    }.bind(this));
+    
     this.resize();
   },
   
   getDimensions: function() {
-    this.left = this.element.positionedOffset().left;
-    this.top = this.element.positionedOffset().top;
-    this.width = this.element.getDimensions().width;
-    this.height = this.element.getDimensions().height;
+    var size = this.ul.getDimensions();
+    this.width = size.width;
+    this.height = size.height;
+  },
+  
+  photoLoaded: function(photo) {
+    var beforeMiddle = Math.floor(this.numPhotos / 2);
+    if (this.photoVisible(photo)) {
+      this.getAspectRatio(photo);
+      this.resize();
+    }
+  },
+  
+  getAspectRatio: function(photo) {
+    if (photo.ratio === undefined) {
+      var measurement = new Element('img', { src: photo.img.src, style: "visibility:hidden;position:absolute" });
+      this.measurer.insert(measurement);
+      var size = measurement.getDimensions();
+      if (size.height > 0) {
+        photo.ratio = size.width / size.height;
+      }
+      measurement.remove();
+    }
   },
   
   resize: function() {
@@ -45,28 +83,42 @@ var PhotoAlbumWidget = Class.create(Widget, {
     this.animate();
   },
   
+  photoVisible: function(photo) {
+    var beforeMiddle = Math.floor(this.numPhotos / 2);
+    return photo.index >= this.currentIndex - beforeMiddle - 1 && photo.index < this.currentIndex + beforeMiddle + 2
+  },
+  
   animate: function() {
     var coordinates = [];
     var beforeMiddle = Math.floor(this.numPhotos / 2);
-    for (var i = 0; i < this.photos.length; ++i) {
+    for (var i = 0, l = this.photos.length; i < l; ++i) {
       var photo = this.photos[i];
+      var visible = this.photoVisible(photo);
+      if (visible) {
+        this.getAspectRatio(photo);
+      }
+
       if (i < this.currentIndex - beforeMiddle) {
-        coordinates = [this.left, this.top + this.height / 2, 0, 0, this.zIndex(i), 0];
+        coordinates = [0, this.height / 2, 0, 0, this.zIndex(i), 0];
       } else if (i >= this.currentIndex - beforeMiddle && i < this.currentIndex + beforeMiddle + 1) {
         var dist = this.currentIndex - i;
         var diff = Math.abs(dist) + 1;
         var scale = Math.sqrt(diff);
         var width = (this.height * photo.ratio) / scale;
         var height = (this.height) / scale;
-        coordinates = [this.left + (this.width / 2) - (dist * (this.width / this.numPhotos)) - (width / 2),
-                       this.top + (this.height - height) / 2,
+        if (width > (this.width / 2)) {
+          width = this.width / 2;
+          height = width / photo.ratio;
+        }
+        coordinates = [(this.width / 2) - (dist * (this.width / this.numPhotos)) - (width / 2),
+                       (this.height - height) / 2,
                        width,
                        height, this.zIndex(i), 1.0 - Math.abs(dist) / (beforeMiddle + 2.0)];
       } else if (i >= this.currentIndex + beforeMiddle) {
-        coordinates = [this.left + this.width, this.top + this.height / 2, 0, 0, this.zIndex(i)];
+        coordinates = [this.width, this.height / 2, 0, 0, this.zIndex(i)];
       }
       
-      if (i >= this.currentIndex - beforeMiddle - 1 && i < this.currentIndex + beforeMiddle + 2) {
+      if (visible) {
         var c = coordinates;
         new Effect.Morph(photo, { style: "left:" + c[0] + "px;top:" + c[1] + "px;width:" + c[2] + "px;height:" + c[3] + "px;opacity:" + c[5], duration: 0.5 });
       } else {
@@ -118,185 +170,3 @@ var PhotoAlbumWidget = Class.create(Widget, {
 });
 
 CurrentPage.registerBehaviour("thc2-photo-album", PhotoAlbumWidget);
-
-
-
-
-
-
-/**
- * reflection.js v1.7
- *
- * Contributors: Cow http://cow.neondragon.net
- *               Gfx http://www.jroller.com/page/gfx/
- *               Sitharus http://www.sitharus.com
- *               Andreas Linde http://www.andreaslinde.de
- *               Tralala, coder @ http://www.vbulletin.org
- *
- * Freely distributable under MIT-style license.
- */
- 
-/* From prototype.js */
-// document.getElementsByClassName = function(className) {
-//  var children = document.getElementsByTagName('*') || document.all;
-//  var elements = new Array();
-//   
-//  for (var i = 0; i < children.length; i++) {
-//    var child = children[i];
-//    var classNames = child.className.split(' ');
-//    for (var j = 0; j < classNames.length; j++) {
-//      if (classNames[j] == className) {
-//        elements.push(child);
-//        break;
-//      }
-//    }
-//  }
-//  return elements;
-// }
-
-var Reflection = {
-	defaultHeight : 0.5,
-	defaultOpacity: 0.5,
-	
-	add: function(image, options) {
-		Reflection.remove(image);
-		
-		doptions = { "height" : Reflection.defaultHeight, "opacity" : Reflection.defaultOpacity }
-		if (options) {
-			for (var i in doptions) {
-				if (!options[i]) {
-					options[i] = doptions[i];
-				}
-			}
-		} else {
-			options = doptions;
-		}
-	
-		try {
-			var d = document.createElement('div');
-			var p = image;
-			
-			var classes = p.className.split(' ');
-			var newClasses = '';
-			for (j=0;j<classes.length;j++) {
-				if (classes[j] != "reflect") {
-					if (newClasses) {
-						newClasses += ' '
-					}
-					
-					newClasses += classes[j];
-				}
-			}
-
-			var reflectionHeight = Math.floor(p.height*options['height']);
-			var divHeight = Math.floor(p.height*(1+options['height']));
-			
-			var reflectionWidth = p.width;
-			
-			if (document.all && !window.opera) {
-				/* Fix hyperlinks */
-                if(p.parentElement.tagName == 'A') {
-	                var d = document.createElement('a');
-	                d.href = p.parentElement.href;
-                }  
-                    
-				/* Copy original image's classes & styles to div */
-				d.className = newClasses;
-				p.className = 'reflected';
-				
-				d.style.cssText = p.style.cssText;
-				p.style.cssText = 'vertical-align: bottom';
-			
-				var reflection = document.createElement('img');
-				reflection.src = p.src;
-				reflection.style.width = reflectionWidth+'px';
-				
-				reflection.style.marginBottom = "-"+(p.height-reflectionHeight)+'px';
-				reflection.style.filter = 'flipv progid:DXImageTransform.Microsoft.Alpha(opacity='+(options['opacity']*100)+', style=1, finishOpacity=0, startx=0, starty=0, finishx=0, finishy='+(options['height']*100)+')';
-				
-				d.style.width = reflectionWidth+'px';
-				d.style.height = divHeight+'px';
-				p.parentNode.replaceChild(d, p);
-				
-				d.appendChild(p);
-				d.appendChild(reflection);
-			} else {
-				var canvas = document.createElement('canvas');
-				if (canvas.getContext) {
-					/* Copy original image's classes & styles to div */
-					d.className = newClasses;
-					p.className = 'reflected';
-					
-					d.style.cssText = p.style.cssText;
-					p.style.cssText = 'vertical-align: bottom';
-			
-					var context = canvas.getContext("2d");
-				
-					canvas.style.height = reflectionHeight+'px';
-					canvas.style.width = reflectionWidth+'px';
-					canvas.height = reflectionHeight;
-					canvas.width = reflectionWidth;
-					
-					d.style.width = reflectionWidth+'px';
-					d.style.height = divHeight+'px';
-					p.parentNode.replaceChild(d, p);
-					
-					d.appendChild(p);
-					d.appendChild(canvas);
-					
-					context.save();
-					
-					context.translate(0,image.height-1);
-					context.scale(1,-1);
-					
-					context.drawImage(image, 0, 0, reflectionWidth, image.height);
-	
-					context.restore();
-					
-					context.globalCompositeOperation = "destination-out";
-					var gradient = context.createLinearGradient(0, 0, 0, reflectionHeight);
-					
-					gradient.addColorStop(1, "rgba(255, 255, 255, 1.0)");
-					gradient.addColorStop(0, "rgba(255, 255, 255, "+(1-options['opacity'])+")");
-		
-					context.fillStyle = gradient;
-					if (navigator.appVersion.indexOf('WebKit') != -1) {
-						context.fill();
-					} else {
-						context.fillRect(0, 0, reflectionWidth, reflectionHeight*2);
-					}
-				}
-			}
-		} catch (e) {
-	    }
-	},
-	
-	remove : function(image) {
-		if (image.className == "reflected") {
-			image.className = image.parentNode.className;
-			image.parentNode.parentNode.replaceChild(image, image.parentNode);
-		}
-	}
-}
-
-function addReflections() {
-	var rimages = document.getElementsByClassName('reflect');
-	for (i=0;i<rimages.length;i++) {
-		var rheight = null;
-		var ropacity = null;
-		
-		var classes = rimages[i].className.split(' ');
-		for (j=0;j<classes.length;j++) {
-			if (classes[j].indexOf("rheight") == 0) {
-				var rheight = classes[j].substring(7)/100;
-			} else if (classes[j].indexOf("ropacity") == 0) {
-				var ropacity = classes[j].substring(8)/100;
-			}
-		}
-		
-		Reflection.add(rimages[i], { height: rheight, opacity : ropacity});
-	}
-}
-
-//var previousOnload = window.onload;
-//window.onload = function () { if(previousOnload) previousOnload(); addReflections(); }
