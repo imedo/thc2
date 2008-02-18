07c8fa81b8e919b0feb6c6900653b259c08e6149
mscherf <mscherf@60cb0372-8f51-4c35-923c-d3d8de73799b>
2008-02-18 15:33:25 +0000

    [IMEDO-1076] "better" bookmark script (1h)
    git-svn-id: file:///var/local/svn/emma@3297 60cb0372-8f51-4c35-923c-d3d8de73799b

--- widgets/bookmark_widget.js
+++ widgets/bookmark_widget.js
@@ -33,11 +33,24 @@ function bookmark(event){
   var url = location.href; 
   var title = document.title;
   
-  if (window.sidebar){
-    window.sidebar.addPanel(title, url, "");
-  } else if(window.opera && window.print){
-    return true;
-  } else if(document.all){
-    window.external.AddFavorite(url, title);
+  // Browser detection
+  var ua=navigator.userAgent.toLowerCase();
+  var isKonq=(ua.indexOf('konqueror')!=-1);
+  var isSafari=(ua.indexOf('webkit')!=-1);
+  var isMac=(ua.indexOf('mac')!=-1);
+  var buttonStr=isMac?'Command/Cmd':'CTRL';
+
+  if(window.external && (!document.createTextNode ||
+    (typeof(window.external.AddFavorite)=='unknown'))) {
+      window.external.AddFavorite(url, title); // IE/Win
+  } else if(isKonq) {
+    alert('You need to press CTRL + B to bookmark our site.');
+  } else if(window.opera || window.home || isSafari) { // Firefox, Netscape, Safari, iCab, Opera
+    alert('You need to press '+buttonStr+' + D to bookmark our site.');
+  } else if(!window.print || isMac) { // IE5/Mac and Safari 1.0
+    alert('You need to press Command/Cmd + D to bookmark our site.');    
+  } else {
+    alert('In order to bookmark this site you need to do so manually through your browser.');
   }
+  
 }
