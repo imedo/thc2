f422a09628520bfb51c32dfbc224c011f31172f4
mscherf <mscherf@60cb0372-8f51-4c35-923c-d3d8de73799b>
2008-04-02 13:52:37 +0000

    new carousel buttons
    git-svn-id: file:///var/local/svn/emma@3626 60cb0372-8f51-4c35-923c-d3d8de73799b

--- widgets/carousel_widget.js
+++ widgets/carousel_widget.js
@@ -10,6 +10,41 @@ var CarouselWidget = Class.create(Widget, {
   initialize: function(element) {
     Widget.prototype.initialize.apply(this, arguments);
     this.carousel = new UI.Carousel($(this.element));
+    this.preview_buttons = $(this.element.down(".preview-buttons"));
+    this.buildPreviewButtons();
+    this.carousel.observe('scroll:ended', this.updatePreviewButtons.bindAsEventListener(this));
+    this.active_preview = $(this.preview_buttons.down(0));
+    this.toggleCurrentActive();
+  },
+  
+  buildPreviewButtons: function(){
+    this.preview_buttons.update('');
+    for(i = 0; i < this.slideCount(); ++i){
+      inserted = this.preview_buttons.insert("<div class='preview-button'>&nbsp;</div>");
+      Event.observe(inserted, 'click', function(){ alert(this.carousel); this.carousel.scrollTo(i*this.perSlide()) }.bind(this));
+    }
+  },
+  
+  perSlide: function(){
+    return Math.floor(this.carousel.nbVisible);
+  },
+  
+  slideCount: function(){
+    return Math.floor(this.carousel.elements.length/this.carousel.nbVisible);
+  },
+  
+  currentSlide: function(){
+    return Math.floor(this.carousel.currentIndex()/this.carousel.nbVisible);
+  },
+  
+  updatePreviewButtons: function(event){
+    this.toggleCurrentActive();
+    this.active_preview = this.preview_buttons.down(this.currentSlide());
+    this.toggleCurrentActive();
+  },
+  
+  toggleCurrentActive: function(){
+    this.active_preview.toggleClassName('preview-button-current');
   }
 
 });
