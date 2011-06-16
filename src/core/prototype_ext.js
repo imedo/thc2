18d96a24d63ff326195bb95ed5f4037902ef59ca
Simon Willmann <simon.willmann@imedo.de>
2011-06-16 18:26:02 +0200

    removed code which should prevent memleaks but breaks in some cases

--- src/core/prototype_ext.js
+++ src/core/prototype_ext.js
@@ -11,38 +11,3 @@ String.prototype.format = function() {
     return str.replace(new RegExp('#\\{' + (i + 1) + '\\}', 'g'), val);
   });
 };
-
-if (Prototype.Browser.IE) {
-  // this actually improves performance in IE 6
-  Event.allEvents = [];
-
-  Event.oldObserve = Event.observe;
-  Event.observe = function(element, eventName, handler) {
-    var event = {
-            element: element,
-            name: eventName,
-            handler: handler
-        };
-
-    Event.allEvents.push(event);
-    Event.oldObserve.apply(Event, arguments);
-  }
-  
-  Element.Methods.observe = Event.observe.methodize();
-  Element.prototype.observe = Event.observe.methodize();
-
-  Event.stopObservingEverything = function() {
-    $A(Event.allEvents).each(function(event) {
-      try {
-        Event.stopObserving(event.element, event.name, event.handler);
-      } finally {
-        event.element = null;
-        event.handler = null;
-      }
-    });
-    Event.allEvents = [];
-    thc2.CurrentPage.initialize();
-  }
-
-  Event.oldObserve(window, "unload", Event.stopObservingEverything.bind(Event));
-}
